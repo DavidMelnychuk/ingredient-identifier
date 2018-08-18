284eb3865e6479e25b186c59f7e29ad438c11563
David Melnychuk <melnychu@sfu.ca>
2018-08-17 18:11:43 -0700

    Add FAB onClick()
    Co-authored-by: Derek Chi <dchi11297@gmail.com>

--- app/src/main/java/app/jade/ingredient_identifier/TakePicActivity.java
+++ app/src/main/java/app/jade/ingredient_identifier/TakePicActivity.java
@@ -2,6 +2,7 @@ package app.jade.ingredient_identifier;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -11,16 +12,26 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
 
 public class TakePicActivity extends AppCompatActivity {
     private static final String TAG = "TakePicActivity";
@@ -38,6 +49,7 @@ public class TakePicActivity extends AppCompatActivity {
     private CameraCaptureSession cameraCaptureSession;
     private CaptureRequest.Builder captureRequestBuilder;
     private CaptureRequest captureRequest;
+    private File galleryFolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +57,8 @@ public class TakePicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_take_pic);
         textureView = findViewById(R.id.texture_view_camera_preview);
 
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA},
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 CAMERA_REQUEST_CODE);
 
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
@@ -95,6 +108,33 @@ public class TakePicActivity extends AppCompatActivity {
 
             }
         };
+
+        createImageGallery();
+
+        FloatingActionButton takePhotoBtn = findViewById(R.id.fab_take_photo);
+        takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lock();
+                FileOutputStream outputPhoto = null;
+                try {
+                    outputPhoto = new FileOutputStream(createImageFile(galleryFolder));
+                    textureView.getBitmap().compress(Bitmap.CompressFormat.PNG, 100, outputPhoto);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    unlock();
+                    try {
+                        if (outputPhoto != null) {
+                            outputPhoto.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
 
     private void createPreviewSession() {
@@ -205,5 +245,38 @@ public class TakePicActivity extends AppCompatActivity {
         backgroundThread = new HandlerThread("camera_background_thread");
         backgroundThread.start();
         backgroundHandler = new Handler(backgroundThread.getLooper());
+    }
+
+    private void createImageGallery() {
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        galleryFolder = new File(storageDirectory, getResources().getString(R.string.app_name));
+        if (!galleryFolder.exists()) {
+            boolean wasCreated = galleryFolder.mkdirs();
+            if (!wasCreated) {
+                Log.e("CapturedImages", "Failed to create directory");
+            }
+        }
+    }
+
+    private File createImageFile(File galleryFolder) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "image_" + timeStamp + "_";
+        return File.createTempFile(imageFileName, ".jpg", galleryFolder);
+    }
+
+    private void lock() {
+        try {
+            cameraCaptureSession.capture(captureRequestBuilder.build(), null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void unlock() {
+        try {
+            cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
