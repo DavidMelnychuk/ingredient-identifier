680bec2adf25746c43116cd5a93221755103c314
Derek Chi <dchi11297@gmail.com>
2018-08-19 15:26:39 -0700

    Add imageLabel and image with progress bar.
    Co-authored-by: David Melnychuk <davidm9779@gmail.com>

--- app/src/main/java/app/jade/ingredient_identifier/IdentifyPhotoActivity.java
+++ app/src/main/java/app/jade/ingredient_identifier/IdentifyPhotoActivity.java
@@ -6,9 +6,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,8 +21,8 @@ import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
 import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class IdentifyPhotoActivity extends AppCompatActivity {
@@ -28,9 +31,9 @@ public class IdentifyPhotoActivity extends AppCompatActivity {
     private Uri imageUri;
     private TextView imageLabel;
     private FirebaseVisionImage image;
-    private List<FirebaseVisionCloudLabel> mFirebaseVisionCloudLabels = new ArrayList<>();
-    private List<String> mFirebaseVisionCloudLabelsStrings = new ArrayList<>();
-    private ListView mListView;
+    private ImageView capturedImage;
+    private Button viewRecipesBtn;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +45,10 @@ public class IdentifyPhotoActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        mListView =  findViewById(R.id.lv_list_of_labels);
         imageLabel = findViewById(R.id.tv_image_label);
+        capturedImage = findViewById(R.id.iv_image);
+        viewRecipesBtn = findViewById(R.id.btn_recipes);
+        mProgressBar = findViewById(R.id.progressBar);
 
 
         FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance().getVisionCloudLabelDetector();
@@ -53,19 +58,15 @@ public class IdentifyPhotoActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(List<FirebaseVisionCloudLabel> firebaseVisionLabels) {
 
+                        FirebaseVisionCloudLabel labelWithHighestConfidence = firebaseVisionLabels.get(0);
 
-                        for (FirebaseVisionCloudLabel label : firebaseVisionLabels) {
-                            String text = label.getLabel();
-                            imageLabel.setText(text);
-                            String entityId = label.getEntityId();
-                            float confidence = label.getConfidence();
-                            mFirebaseVisionCloudLabelsStrings.add(text + "---" + entityId + "---" + confidence);
-                        }
 
-                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(IdentifyPhotoActivity.this,
-                                android.R.layout.simple_list_item_1, //layout to use
-                                mFirebaseVisionCloudLabelsStrings);//Item to be display
-                        mListView.setAdapter(adapter);
+                        imageLabel.setText(labelWithHighestConfidence.getLabel() + ", Confidence: " + (100 * labelWithHighestConfidence.getConfidence()) + "%");
+
+                        setUpImageView();
+
+                        updateUI();
+
 
                     }
                 })
@@ -76,6 +77,21 @@ public class IdentifyPhotoActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+
+
+    private void setUpImageView() {
+        File imgFile = new File(imageUri.getPath());
+        if (imgFile.exists()) {
+            capturedImage.setImageURI(imageUri);
+        }
+    }
+
+    private void updateUI() {
+        mProgressBar.setVisibility(View.GONE);
+        imageLabel.setVisibility(View.VISIBLE);
+        capturedImage.setVisibility(View.VISIBLE);
+        viewRecipesBtn.setVisibility(View.VISIBLE);
     }
 
 
