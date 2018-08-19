90caa4881a8f354cc19b94d5f848d820185518ca
Derek Chi <dchi11297@gmail.com>
2018-08-18 17:15:29 -0700

    Add Firebase Cloud Vision Detection. Need to fix.
    Co-authored-by: David Melnychuk <davidm9779@gmail.com>

--- app/src/main/java/app/jade/ingredient_identifier/IdentifyPhotoActivity.java
+++ app/src/main/java/app/jade/ingredient_identifier/IdentifyPhotoActivity.java
@@ -1,13 +1,85 @@
 package app.jade.ingredient_identifier;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IdentifyPhotoActivity extends AppCompatActivity {
+
+    private static final String IMAGE_URI = "imageUri";
+    private Uri imageUri;
+    private TextView imageLabel;
+    private FirebaseVisionImage image;
+    private List<FirebaseVisionCloudLabel> mFirebaseVisionCloudLabels = new ArrayList<>();
+    private List<String> mFirebaseVisionCloudLabelsStrings = new ArrayList<>();
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify_photo);
+        imageUri = Uri.parse(getIntent().getStringExtra(IMAGE_URI));
+        try {
+            image = FirebaseVisionImage.fromFilePath(this, imageUri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mListView =  findViewById(R.id.lv_list_of_labels);
+
+
+        FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance().getVisionCloudLabelDetector();
+
+        Task<List<FirebaseVisionCloudLabel>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionCloudLabel> firebaseVisionLabels) {
+
+
+                        for (FirebaseVisionCloudLabel label : firebaseVisionLabels) {
+                            String text = label.getLabel();
+                            String entityId = label.getEntityId();
+                            float confidence = label.getConfidence();
+                            mFirebaseVisionCloudLabelsStrings.add(text + "---" + entityId + "---" + confidence);
+                        }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(IdentifyPhotoActivity.this,
+                                android.R.layout.simple_list_item_1, //layout to use
+                                mFirebaseVisionCloudLabelsStrings);//Item to be display
+                        mListView.setAdapter(adapter);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+    }
+
+
+    public static Intent makeLaunchIntent(Context context, Uri imageUri) {
+        Intent intent = new Intent(context, IdentifyPhotoActivity.class);
+        intent.putExtra(IMAGE_URI, imageUri.toString());
+        return intent;
     }
 }
